22550dba72b07f78c3c239816a0533ee3790fb57
Sadik Saifi <Sadiksaifi205@gmail.com>
2023-02-10 23:06:47 +0530

    Fix autologin issue.

--- arch_install3.sh
+++ arch_install3.sh
@@ -49,14 +49,22 @@ git remote set-url --push origin git@github.com:sadikeey/dmenu.git
 cd $HOME/.config/emacs
 git remote set-url --push origin git@github.com:sadikeey/emacs.git
 
-[ -d "/etc/systemd/system/getty@tty1.service.d/" ] || sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
-sudo cp $HOME/.dotfiles/.misc/autologin.conf
-
 # Setting Wallpaper
 cp $HOME/.dotfiles/.misc/wall.jpg $HOME/.config/
 
 rm $HOME/bash*
 mv $HOME/.icons $HOME/.local/share/icons
+
+sudo su
+echo "Enter your username for autologin: "
+read $username
+
+[ -d "/etc/systemd/system/getty@tty1.service.d/" ] || mkdir -p /etc/systemd/system/getty@tty1.service.d/
+touch "/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+echo "[Services]" > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "ExecStart=-/sbin/agetty -o '-p -f -- \\u' --noclear --autologin sdk %I $TERM" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+echo "Type=simple" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
 
 echo "#################################################"
 echo "## You have successfully installed the system! ##"
